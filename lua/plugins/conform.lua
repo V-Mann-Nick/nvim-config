d2551effa2dc7ce87972a35edc34b906fe6edb9a
Nicklas Sedlock <nicklas.sedlock@posteo.net>
2025-10-29 11:43:39 +0100

    fix: remove dioxus formatter (was causing RA re-index)

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -40,7 +40,7 @@ return {
             typescript = { "prettierd", "prettier", stop_after_first = true },
             typescriptreact = { "prettierd", "prettier", stop_after_first = true },
             javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-            rust = { "rustfmt", "dioxus" },
+            rust = { "rustfmt" },
         },
     },
 }
