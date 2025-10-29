6a3106a9d1870344ae85726e3e2fa116942758e1
Nicklas Sedlock <nicklas.sedlock@posteo.net>
2025-10-29 11:42:54 +0100

    feat: .telescope-ignore

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -77,7 +77,18 @@ return {
         vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
         vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
         vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sg", function()
+            local ignore_file = vim.fn.getcwd() .. "/.telescope-ignore"
+            local additional_args = {}
+
+            if vim.fn.filereadable(ignore_file) == 1 then
+                additional_args = { "--ignore-file", ignore_file }
+            end
+
+            builtin.live_grep({
+                additional_args = additional_args
+            })
+        end, { desc = "[S]earch by [G]rep" })
         vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
         vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
         vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
